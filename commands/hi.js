f18bd66f2c615c1588e6a82b62370fd8f63936f3
KooliswYoutube <71171701+KooliswYoutube@users.noreply.github.com>
2020-09-12 17:06:39 +0800

    Update hi.js

--- commands/hi.js
+++ commands/hi.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js')
 
-module.export.run = async (client, message, args) => {
+module.exports.run = async (client, message, args) => {
    message.channel.send("hi")
 }
